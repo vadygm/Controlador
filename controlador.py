23b97703474377bdd2ee7c9a32117c3e1e305e5c
JoelIsaac0 <118613482+JoelIsaac0@users.noreply.github.com>
2023-12-17 21:46:48 -0500

    Actualizacion de creacion de cuenta de ahorros

--- controlador.py
+++ controlador.py
@@ -1,10 +1,10 @@
-#installamos el paquete psycopg2 para poder realizar conexion a nuestra base de datos en potsgres
+# Importamos las bibliotecas necesarias
 import psycopg2
 import re
+import random
 from psycopg2 import sql
 
-
-#Agregamos la conexion a nuestra base de datos de potsgres
+# Conectamos a la base de datos de PostgreSQL
 conexion = psycopg2.connect(
     database="usuario",
     user="postgres",
@@ -12,38 +12,35 @@ conexion = psycopg2.connect(
     host="localhost",
     port="5432"
 )
-#para poder agregar sentencias SQL en nuestro codigo de pyhton agregamos un cursos el cual 
-#permite realizar sentencias SQL
+
+# Creamos un cursor para ejecutar las consultas SQL
 cursor = conexion.cursor()
 
-#Creamos nuestro usuario si en nuestro potsgres aun no lo hemos realizado caso contrario
-#lo dejamos comentado.
-def crear_tablas():
-    crear_usuarios_query = """
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id SERIAL PRIMARY KEY,
-        nombre VARCHAR(50),
-        apellido VARCHAR(50),
-        correo VARCHAR(100),
-        cedula VARCHAR(20),
-        celular VARCHAR(15)
-    );
-    """
+# Creamos la tabla de usuarios si no existe
+crear_tabla_query = """
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    correo VARCHAR(100),
+    cedula VARCHAR(20),
+    celular VARCHAR(15)
+);
+"""
+cursor.execute(crear_tabla_query)
 
-    crear_cuentas_query = """
-    CREATE TABLE IF NOT EXISTS cuentas_ahorro (
-        id SERIAL PRIMARY KEY,
-        usuario_id INTEGER UNIQUE REFERENCES usuarios(id),
-        saldo DECIMAL(10, 2) DEFAULT 0.0
-    );
-    """
+# Creamos la tabla de cuentas de ahorros si no existe
+crear_tabla_cuentas_query = """
+CREATE TABLE IF NOT EXISTS cuentas_ahorros (
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER REFERENCES usuarios(id),
+    numero_cuenta VARCHAR(20) UNIQUE,
+    saldo DECIMAL(10, 2) 
+);
+"""
+cursor.execute(crear_tabla_cuentas_query)
 
-    cursor.execute(crear_usuarios_query)
-    cursor.execute(crear_cuentas_query)
-    conexion.commit()
-    
-#Desarollamos una funcion para poder agregar nuevos usuarios a nuestra base de datos
-#llenando los datos creados en nuestra tabla usuarios
+# Función para agregar un usuario a la base de datos
 def agregar_usuario(nombre, apellido, correo, cedula, celular):
     # Validamos que todos los datos estén ingresados
     if not nombre or not apellido or not correo or not cedula or not celular:
@@ -60,6 +57,7 @@ def agregar_usuario(nombre, apellido, correo, cedula, celular):
         print("El número de celular debe contener 10 dígitos numéricos")
         return None
 
+    # Insertamos el usuario en la tabla de usuarios
     insertar_usuario_query = """
     INSERT INTO usuarios (nombre, apellido, correo, cedula, celular)
     VALUES (%s, %s, %s, %s, %s) RETURNING id;
@@ -69,30 +67,29 @@ def agregar_usuario(nombre, apellido, correo, cedula, celular):
     conexion.commit()
     return usuario_id
 
-def crear_cuenta_ahorro(usuario_id):
-    try:
-        crear_cuenta_query = """
-        INSERT INTO cuentas_ahorro (usuario_id) VALUES (%s) RETURNING id;
-        """
-        cursor.execute(crear_cuenta_query, (usuario_id,))
-        cuenta_id = cursor.fetchone()[0]
-        conexion.commit()
-        return cuenta_id
-    except psycopg2.IntegrityError:
-        # Si hay una violación de la restricción UNIQUE, significa que el usuario ya tiene una cuenta
-        print("Este usuario ya tiene una cuenta de ahorro.")
-        return None
+# Función para crear una cuenta de ahorros
+def crear_cuenta_ahorros(usuario_id, saldo_inicial=0):
+    # Generamos un número de cuenta aleatorio
+    numero_cuenta = ''.join([str(random.randint(0, 9)) for _ in range(10)])
 
-#Creamos una funcion para poder realizar una sentecia sql a la base de datos
-#en este casio una sentencias para obtener los usuarios de una bbdd.
+    # Insertamos la cuenta de ahorros en la base de datos
+    insertar_cuenta_query = """
+    INSERT INTO cuentas_ahorros (usuario_id, numero_cuenta, saldo)
+    VALUES (%s, %s, %s) RETURNING id;
+    """
+    cursor.execute(insertar_cuenta_query, (usuario_id, numero_cuenta, saldo_inicial))
+    cuenta_id = cursor.fetchone()[0]
+    conexion.commit()
+    return cuenta_id
+
+# Función para obtener todos los usuarios de la base de datos
 def obtener_usuarios():
     obtener_usuarios_query = "SELECT * FROM usuarios;"
     cursor.execute(obtener_usuarios_query)
     usuarios = cursor.fetchall()
     return usuarios
 
-#Creamos esta funcion para poder visualizar en consola que la conexion sea exitosa
-#y la creacion, validacion de datos de un usuario sean correctos
+# Función para mostrar datos de un usuario
 def datos_usuario():
     while True:
         nombre = input("Ingrese el nombre: ")
@@ -107,35 +104,29 @@ def datos_usuario():
         else:
             print("Por favor ingrese todos sus datos correctamente")
 
-#si algun elemento fue eliminado restabl3ecemos los id
-def resetear_ids():
-    resetear_ids_query = "TRUNCATE TABLE usuarios RESTART IDENTITY;"
-    cursor.execute(resetear_ids_query)
-    conexion.commit()
-
-#creamos nuestro main donde ejecutaremos nuestras funciones realizadas
-#como lo es la creacion de un usuario y visualizacion del mismo en la bbdd si la conexion es exitosa
 if __name__ == "__main__":
-    #Ingresamos un nuevo usuario por consola validando el mismo
-    
-    crear_tablas()
-    
+    # Ingresamos un nuevo usuario por consola validando el mismo
     datos_usuario = datos_usuario()
     nuevo_usuario_id = agregar_usuario(*datos_usuario)
 
     if nuevo_usuario_id:
-        nuevo_cuenta_id = crear_cuenta_ahorro(nuevo_usuario_id)
-        if nuevo_cuenta_id is not None:
-            print(f"Nuevo usuario agregado con ID: {nuevo_usuario_id} y cuenta de ahorro creada con ID: {nuevo_cuenta_id}")
+        # Traemos el id del usuario
+        print(f"Nuevo usuario agregado con ID: {nuevo_usuario_id}")
 
+        # Creamos una cuenta de ahorros asociada al nuevo usuario
+        nueva_cuenta_id = crear_cuenta_ahorros(nuevo_usuario_id, saldo_inicial=20)  # Puedes ajustar el saldo inicial
 
-    #Imprimimos todo los usuarios ingresados en nuestra bbdd
-    usuarios = obtener_usuarios()
-    print("\nLista de Usuarios:")
-    for usuario in usuarios:
-        print(usuario)
-    #llamamos la metodo para recetear los ids
-    #resetear_ids()
-    #Cerramos finalmente nuestra conexion para que sea segura
+        if nueva_cuenta_id:
+            print(f"Se ha creado una cuenta de ahorros con ID: {nueva_cuenta_id}")
+
+    # Imprimimos todas las cuentas de ahorros
+    cuentas_ahorros_query = "SELECT * FROM cuentas_ahorros;"
+    cursor.execute(cuentas_ahorros_query)
+    cuentas_ahorros = cursor.fetchall()
+    print("\nLista de Cuentas de Ahorros:")
+    for cuenta in cuentas_ahorros:
+        print(cuenta)
+
+    # Cerramos finalmente nuestra conexión para que sea segura
     cursor.close()
     conexion.close()
