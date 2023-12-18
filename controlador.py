82197514d0b9d3dc4bf03870e7b0f86add80f261
vadygm <richard_vady1998@hotmail.com>
2023-12-18 00:05:45 -0500

    parte 3

--- controlador.py
+++ controlador.py
@@ -1,10 +1,9 @@
-#installamos el paquete psycopg2 para poder realizar conexion a nuestra base de datos en potsgres
 import psycopg2
-import re
 from psycopg2 import sql
+import re
+import random
 
-
-#Agregamos la conexion a nuestra base de datos de potsgres
+# Conexión a la base de datos
 conexion = psycopg2.connect(
     database="usuario",
     user="postgres",
@@ -12,12 +11,11 @@ conexion = psycopg2.connect(
     host="localhost",
     port="5432"
 )
-#para poder agregar sentencias SQL en nuestro codigo de pyhton agregamos un cursos el cual 
-#permite realizar sentencias SQL
+
+# Cursor para ejecutar consultas SQL
 cursor = conexion.cursor()
 
-#Creamos nuestro usuario si en nuestro potsgres aun no lo hemos realizado caso contrario
-#lo dejamos comentado.
+# Creamos la tabla de usuarios si no existe
 crear_tabla_query = """
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
@@ -30,8 +28,49 @@ CREATE TABLE IF NOT EXISTS usuarios (
 """
 cursor.execute(crear_tabla_query)
 
-#Desarollamos una funcion para poder agregar nuevos usuarios a nuestra base de datos
-#llenando los datos creados en nuestra tabla usuarios
+# Agrega esta parte al código donde defines las tablas
+crear_tabla_transacciones_query = """
+CREATE TABLE IF NOT EXISTS transacciones (
+    id SERIAL PRIMARY KEY,
+    tipo VARCHAR(20),
+    descripcion TEXT,
+    monto DECIMAL(10, 2),
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usuario_id INTEGER REFERENCES usuarios(id),
+    cuenta_id INTEGER REFERENCES cuentas_ahorros(id)
+);
+"""
+cursor.execute(crear_tabla_transacciones_query)
+
+crear_tabla_creditos_query = """
+CREATE TABLE IF NOT EXISTS creditos (
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER,
+    cuenta_id INTEGER,
+    monto DECIMAL(10, 2),
+    plazo_meses INTEGER,
+    tasa_interes DECIMAL(5, 4),
+    estado VARCHAR(20) DEFAULT 'PENDIENTE'
+);
+"""
+cursor.execute(crear_tabla_creditos_query)
+
+crear_tabla_amortizacion_query = """
+CREATE TABLE IF NOT EXISTS amortizacion_creditos (
+    id SERIAL PRIMARY KEY,
+    credito_id INTEGER,
+    mes INTEGER,
+    cuota DECIMAL(10, 2),
+    interes DECIMAL(10, 2),
+    amortizacion DECIMAL(10, 2),
+    saldo_pendiente DECIMAL(10, 2)
+);
+"""
+cursor.execute(crear_tabla_amortizacion_query)
+
+conexion.commit()
+
+
 def agregar_usuario(nombre, apellido, correo, cedula, celular):
     # Validamos que todos los datos estén ingresados
     if not nombre or not apellido or not correo or not cedula or not celular:
@@ -48,6 +87,7 @@ def agregar_usuario(nombre, apellido, correo, cedula, celular):
         print("El número de celular debe contener 10 dígitos numéricos")
         return None
 
+    # Insertamos el usuario en la tabla de usuarios
     insertar_usuario_query = """
     INSERT INTO usuarios (nombre, apellido, correo, cedula, celular)
     VALUES (%s, %s, %s, %s, %s) RETURNING id;
@@ -57,54 +97,142 @@ def agregar_usuario(nombre, apellido, correo, cedula, celular):
     conexion.commit()
     return usuario_id
 
-#Creamos una funcion para poder realizar una sentecia sql a la base de datos
-#en este casio una sentencias para obtener los usuarios de una bbdd.
-def obtener_usuarios():
-    obtener_usuarios_query = "SELECT * FROM usuarios;"
-    cursor.execute(obtener_usuarios_query)
-    usuarios = cursor.fetchall()
-    return usuarios
+# Agrega estas funciones
 
-#Creamos esta funcion para poder visualizar en consola que la conexion sea exitosa
-#y la creacion, validacion de datos de un usuario sean correctos
-def datos_usuario():
-    while True:
-        nombre = input("Ingrese el nombre: ")
-        apellido = input("Ingrese el apellido: ")
-        correo = input("Ingrese el correo electrónico: ")
-        cedula = input("Ingrese la cédula (10 dígitos numéricos): ")
-        celular = input("Ingrese el número de celular (10 dígitos numéricos): ")
+def solicitar_credito(usuario_id, monto, plazo_meses, tasa_interes):
+    # Se supone que usuario_id y cuenta_id deberían ser diferentes.
+    # En este ejemplo, estoy utilizando el mismo usuario_id como cuenta_id.
+    cuenta_id = usuario_id
 
-        # Validamos los datos del usuario
-        if nombre and apellido and correo and re.match(r'^[0-9]{10}$', cedula) and re.match(r'^[0-9]{10}$', celular):
-            return nombre, apellido, correo, cedula, celular
-        else:
-            print("Por favor ingrese todos sus datos correctamente")
-
-#si algun elemento fue eliminado restabl3ecemos los id
-def resetear_ids():
-    resetear_ids_query = "TRUNCATE TABLE usuarios RESTART IDENTITY;"
-    cursor.execute(resetear_ids_query)
+    insertar_credito_query = """
+    INSERT INTO creditos (usuario_id, cuenta_id, monto, plazo_meses, tasa_interes)
+    VALUES (%s, %s, %s, %s, %s) RETURNING id;
+    """
+    cursor.execute(insertar_credito_query, (usuario_id, cuenta_id, monto, plazo_meses, tasa_interes))
+    credito_id = cursor.fetchone()[0]
     conexion.commit()
+    return credito_id
 
-#creamos nuestro main donde ejecutaremos nuestras funciones realizadas
-#como lo es la creacion de un usuario y visualizacion del mismo en la bbdd si la conexion es exitosa
-if __name__ == "__main__":
-    #Ingresamos un nuevo usuario por consola validando el mismo
-    datos_usuario = datos_usuario()
-    nuevo_usuario_id = agregar_usuario(*datos_usuario)
+def obtener_creditos_usuario(usuario_id):
+    obtener_creditos_query = """
+    SELECT *
+    FROM creditos
+    WHERE usuario_id = %s;
+    """
+    cursor.execute(obtener_creditos_query, (usuario_id,))
+    creditos_usuario = cursor.fetchall()
+    return creditos_usuario
 
-    if nuevo_usuario_id:
-        #traemos el id del usuario
-        print(f"Nuevo usuario agregado con ID: {nuevo_usuario_id}")
+def aprobar_credito(credito_id):
+    aprobar_credito_query = """
+    UPDATE creditos
+    SET estado = 'APROBADO'
+    WHERE id = %s;
+    """
+    cursor.execute(aprobar_credito_query, (credito_id,))
+    conexion.commit()
+    print(f"Crédito con ID {credito_id} aprobado")
 
-    #Imprimimos todo los usuarios ingresados en nuestra bbdd
-    usuarios = obtener_usuarios()
-    print("\nLista de Usuarios:")
-    for usuario in usuarios:
-        print(usuario)
-    #llamamos la metodo para recetear los ids
-    #resetear_ids()
-    #Cerramos finalmente nuestra conexion para que sea segura
-    cursor.close()
-    conexion.close()
+def rechazar_credito(credito_id):
+    # Actualizamos el estado del crédito a Rechazado
+    rechazar_query = "UPDATE creditos SET estado = 'Rechazado' WHERE id = %s;"
+    cursor.execute(rechazar_query, (credito_id,))
+    conexion.commit()
+    print(f"Crédito con ID {credito_id} rechazado")    
+
+def generar_reporte_creditos(usuario_id):
+    creditos_usuario = obtener_creditos_usuario(usuario_id)
+
+    if creditos_usuario:
+        print(f"Reporte de créditos para el usuario con ID {usuario_id}:")
+        for credito in creditos_usuario:
+            print(credito)
+    else:
+        print(f"No hay créditos para el usuario con ID {usuario_id}")
+
+def generar_tabla_amortizacion(credito_id):
+    # Obtener información del crédito
+    obtener_credito_query = """
+    SELECT monto, plazo_meses, tasa_interes
+    FROM creditos
+    WHERE id = %s;
+    """
+    cursor.execute(obtener_credito_query, (credito_id,))
+    credito_info = cursor.fetchone()
+
+    if not credito_info:
+        print("No se encontró información del crédito.")
+        return
+
+    monto, plazo_meses, tasa_interes = credito_info
+
+    # Calcular la cuota mensual
+    tasa_mensual = tasa_interes / 12
+    cuota_mensual = (monto * tasa_mensual) / (1 - (1 + tasa_mensual)**-plazo_meses)
+
+    # Inicializar la tabla de amortización
+    tabla_amortizacion = []
+    saldo_pendiente = monto
+
+    for mes in range(1, plazo_meses + 1):
+        interes_mes = saldo_pendiente * tasa_mensual
+        amortizacion_mes = cuota_mensual - interes_mes
+        saldo_pendiente -= amortizacion_mes
+
+        fila_amortizacion = {
+            "Mes": mes,
+            "Cuota": cuota_mensual,
+            "Interés": interes_mes,
+            "Amortización": amortizacion_mes,
+            "Saldo Pendiente": saldo_pendiente
+        }
+
+        tabla_amortizacion.append(fila_amortizacion)
+
+    # Guardar la información en una tabla amortizacion_creditos
+    guardar_amortizacion_query = """
+    INSERT INTO amortizacion_creditos (credito_id, mes, cuota, interes, amortizacion, saldo_pendiente)
+    VALUES (%s, %s, %s, %s, %s, %s);
+    """
+    for fila in tabla_amortizacion:
+        cursor.execute(guardar_amortizacion_query, (credito_id, fila["Mes"], fila["Cuota"], fila["Interés"], fila["Amortización"], fila["Saldo Pendiente"]))
+
+    conexion.commit()
+    print("Tabla de amortización generada y guardada.")
+
+
+# Ejemplo de uso:
+# Agregar un usuario
+nombre = input("Ingrese el nombre: ")
+apellido = input("Ingrese el apellido: ")
+correo = input("Ingrese el correo electrónico: ")
+cedula = input("Ingrese la cédula (10 dígitos numéricos): ")
+celular = input("Ingrese el número de celular (10 dígitos numéricos): ")
+
+usuario_id = agregar_usuario(nombre, apellido, correo, cedula, celular)
+print(f"Nuevo usuario agregado con ID: {usuario_id}")
+
+# Solicitar un crédito para el usuario
+monto = float(input("Ingrese el monto del crédito: "))
+plazo_meses = int(input("Ingrese el plazo del crédito en meses: "))
+tasa_interes = float(input("Ingrese la tasa de interés del crédito (porcentaje): "))
+
+credito_id = solicitar_credito(usuario_id, monto, plazo_meses, tasa_interes)
+print(f"Crédito solicitado con ID: {credito_id}")
+
+# Aprobar el crédito
+aprobar_credito(credito_id)
+print(f"Crédito con ID {credito_id} aprobado")
+
+# Obtener la lista de créditos
+creditos = obtener_creditos_usuario(usuario_id)
+print("\nLista de Créditos:")
+for credito in creditos:
+    print(credito)
+
+# Generar tabla de amortización
+generar_tabla_amortizacion(credito_id)
+
+# Cerramos finalmente nuestra conexión para que sea segura
+cursor.close()
+conexion.close()
